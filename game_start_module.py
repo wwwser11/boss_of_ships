ccedc23d2130a15b24f88ab45a6eea32d09eea97
Sergey Garanin <wwwser11@yandex.ru>
2021-03-24 20:39:13 +0300

    add mobs

--- game_start_module.py
+++ game_start_module.py
@@ -2,6 +2,7 @@ import pygame
 import random
 from Player_class import *
 from game_cycle import *
+from Mob_class import *
 
  # size
 WIDTH = 480
@@ -18,6 +19,7 @@ BLUE = (0, 0, 255)
 # add color of temporary player img rectangle and back
 back_color = BLACK
 pcolor = GREEN
+mcolor = BLUE
 
 pygame.init() # turn on pygame
 pygame.mixer.init() # music
@@ -26,7 +28,12 @@ pygame.display.set_caption('BOSS_of_SHIPS')
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
 player = Player(WIDTH, HEIGHT, pcolor)
 all_sprites.add(player)
+for i in range(8):
+    m = Mob(mcolor, WIDTH, HEIGHT)
+    all_sprites.add(m)
+    mobs.add(m)
 
 game(FPS, screen, back_color, clock, all_sprites)
