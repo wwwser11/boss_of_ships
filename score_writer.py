01ffae90c2ec7f0ea12f46a0ec87e55d523a1c22
Sergey Garanin <wwwser11@yandex.ru>
2021-04-02 20:52:58 +0300

    add font inside game to faster load

--- score_writer.py
+++ score_writer.py
@@ -1,17 +1,14 @@
 import pygame
 
 # func to draw score
-font_name = pygame.font.match_font('arial')
-
-
-
 def draw_text(surf, text, size, x, y, colors):
-    font = pygame.font.Font(font_name, size)
+    font = pygame.font.Font('font/a_FuturaRound Bold.ttf', size)
     # 'True' turn on anti-aliased
     text_surface = font.render(text, True, colors['WHITE'])
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
+
 
 def draw_health(surf, x, y, player_shiald, colors):
     if player_shiald < 0:
